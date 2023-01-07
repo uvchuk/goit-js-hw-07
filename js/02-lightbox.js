0c6ddf475346c0f35ad781fc083aeac692a4f808
uvchuk <uvchuk@gmail.com>
2023-01-07 16:49:44 +0100

    beta release second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const createMarkup = markup();
+const gallery = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+    captionDelay: 250,
+    
+});
+
+function markup() {
+	const markupArr = galleryItems
+		.map(({original, preview, description}) => {
+			return `<a class="gallery__item" href="${original}"><img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/></a>`;
+		})
+		.join("");
+	galleryRef.insertAdjacentHTML("afterbegin", markupArr);
+}
