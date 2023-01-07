b037967c07a6314b6ae46394fce3ee2c18a05009
uvchuk <uvchuk@gmail.com>
2023-01-07 16:12:32 +0100

    beta release first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const createMarkup = markup();
+const showModal = modal();
+
+function markup() {
+	const markupArr = galleryItems
+		.map(({original, preview, description}) => {
+			return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"></img></div>`;
+		})
+		.join("");
+	galleryRef.insertAdjacentHTML("afterbegin", markupArr);
+	galleryRef.addEventListener("click", evt => evt.preventDefault());
+}
+
+function modal() {
+	galleryRef.addEventListener("click", evt => {
+		if (evt.target.nodeName === "IMG") {
+			evt.target.setAttribute("src", evt.target.dataset.source);
+			const instance = basicLightbox.create(evt.target.outerHTML);
+			instance.show();
+		}
+	});
+}
